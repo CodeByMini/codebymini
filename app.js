b7086920e66513f3ae0e2cfe572c55f253b5af59
CodeByMini <caspian@lightsbymini.se>
2021-04-12 11:31:04 +0200

    alot of stuff

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/', function(req, res) {
     res.render("index", {data: data});
 });
 
+app.get('/cv', function(req, res) {
+    res.render("cv", {data: data});
+});
+
+app.get('/contact', function(req, res) {
+    res.render("contact", {data: data});
+});
+
 app.listen(process.env.PORT, function() {
     console.log("listening on port " + process.env.PORT);
 })
